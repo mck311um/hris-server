6917cff3e3fcffc705306f0135422753fe322fd2
Mc Kellum <mck311um@gmail.com>
2024-10-09 13:17:02 -0400

    fix(Attendance Job): Change CRON job for attendance job

--- jobs/attendanceJob.js
+++ jobs/attendanceJob.js
@@ -93,7 +93,7 @@ async function getTestClientAttendance() {
   }
 }
 
-cron.schedule("* * * * *", () => {
+cron.schedule("0 8 * * *", () => {
   console.log("Running Attendance Job");
   getTestClientAttendance();
   console.log("Job Completed Successfully");
